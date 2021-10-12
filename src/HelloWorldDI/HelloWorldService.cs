b78c03533aedaa19e95a5d625ba522ecf5af68bc
Just15 <Just15@users.noreply.github.com>
2021-10-12 15:42:09 -0600

    Initial

---
+++ src/HelloWorldDI/HelloWorldService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldDI
+{
+    public class HelloWorldService
+    {
+        public void Write(string name)
+        {
+            Console.WriteLine($"Hello {name}!");
+        }
+    }
+}
---
+++ src/HelloWorldDI/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HelloWorldDI
+{
+    internal class Program
+    {
+        private readonly HelloWorldService helloWorldService;
+
+        private static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            host.Services.GetRequiredService<Program>().Run();
+        }
+
+        public Program(HelloWorldService helloWorldService)
+        {
+            this.helloWorldService = helloWorldService;
+        }
+
+        private void Run()
+        {
+            helloWorldService.Write("Just15");
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddScoped<Program>();
+                    services.AddScoped<HelloWorldService>();
+                });
+        }
+    }
+}
